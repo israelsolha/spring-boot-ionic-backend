495c3f4765b2d973acba6823366f8cef7659d2f8
Israel <israel-solha@hotmail.com>
2021-02-09 21:48:48 -0300

    Criação de Repository e Service para a Categoria

---
+++ src/main/java/com/israelsolha/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.israelsolha.cursomc.repositories;
+
+import com.israelsolha.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria,Integer> {
+}

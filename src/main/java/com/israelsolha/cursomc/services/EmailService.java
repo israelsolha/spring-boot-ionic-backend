49d8169b954f4aef1d60e988462bcfb1bc74fdcb
Israel <israel-solha@hotmail.com>
2021-02-17 18:37:12 -0300

    MockEmailSerivce com Logger. Padroes Strategy e Template Method

---
+++ src/main/java/com/israelsolha/cursomc/services/EmailService.java
@@ -0,0 +1,11 @@
+package com.israelsolha.cursomc.services;
+
+import com.israelsolha.cursomc.domain.Pedido;
+import org.springframework.mail.SimpleMailMessage;
+
+public interface EmailService {
+
+    void sendOrderConfirmationEmail(Pedido obj);
+
+    void sendEmail(SimpleMailMessage msg);
+}
